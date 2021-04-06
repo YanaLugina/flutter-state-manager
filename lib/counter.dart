954de448898c427dc45ec3daec3629d609ac3a73
YanaLugina <yana4961@gmail.com>
2021-04-07 00:33:00 +1000

    scoped: used scoped model lib for state

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_state_manager/main.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final rootWidgetState = MyInheritedWidget.of(context)!.myState;
-    return Card(
-      margin: EdgeInsets.all(4.0).copyWith(bottom: 32),
-      color: Colors.yellowAccent,
-      child: Column(
-        children: <Widget>[
-          Text('(Children Widget 2)'),
-          Text('(${rootWidgetState.counterValue})', style: Theme.of(context).textTheme.headline4),
-          ButtonBar(
-            children: <Widget>[
-              IconButton(
-                  onPressed: () => rootWidgetState.decrementCounter(),
-                  icon: Icon(Icons.remove),
-                  color: Colors.red
-              ),
-              IconButton(
-                onPressed: () => rootWidgetState.incrementCounter(),
-                icon: Icon(Icons.add),
-                color: Colors.green,
-              ),
-            ],
-          )
-        ],
-      ),
+    return ScopedModelDescendant<MyModelState>(
+      rebuildOnChange: true,
+      builder: (context, child, model) => Card(
+        margin: EdgeInsets.all(4.0).copyWith(bottom: 32),
+        color: Colors.yellowAccent,
+        child: Column(
+          children: <Widget>[
+            Text('(Children Widget 2)'),
+            Text('(${model.counterValue})', style: Theme.of(context).textTheme.headline4),
+            ButtonBar(
+              children: <Widget>[
+                IconButton(
+                    onPressed: () => model.decrementCounter(),
+                    icon: Icon(Icons.remove),
+                    color: Colors.red
+                ),
+                IconButton(
+                  onPressed: () => model.incrementCounter(),
+                  icon: Icon(Icons.add),
+                  color: Colors.green,
+                ),
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
